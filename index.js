fd547489df2e185eb4f811d18ee20a5fee14876f
Alexandre Vieira <AlexandreV1@bitbucket.org>
2020-04-15 23:53:39 -0300

    added frontend

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express();
 app.use(express.urlencoded());
 app.use(express.json());
 app.use("/", require("./routes"));
+app.use("/static", express.static("public"));
 
 app.listen(3000, "0.0.0.0");
